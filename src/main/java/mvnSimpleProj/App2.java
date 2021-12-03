a2663bacff29a4eec526345163b55c7cd358c694
varnit rohilla <L00163087@student.lyit.ie>
2021-12-03 15:27:57 +0000

    Addded a maven commit

---
+++ src/main/java/mvnSimpleProj/App2.java
@@ -0,0 +1,14 @@
+package mvnSimpleProj;
+
+public class App2 {
+
+	public App2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("changed for the workflow");
+	}
+
+}
